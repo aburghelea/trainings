e96eaa81378e0565834ef5ac82f6fdbe1e5c4334
Alexandru Burghelea <iceman.ftg@gmail.com>
2012-07-11 15:56:03 +0300

    Starting XML Hierarchy

--- spring-training/src/main/java/ro/teamnet/springtraining/TheGreedyCapitalist.java
+++ spring-training/src/main/java/ro/teamnet/springtraining/TheGreedyCapitalist.java
@@ -8,36 +8,46 @@ package ro.teamnet.springtraining;
  * TNI
  */
 public class TheGreedyCapitalist {
+    public static final String DEFAULT = "USA";
     /**
      * package protect (without modifier)
      */
     Worker chineseDude;
 
+    private String country = DEFAULT;
+
     private TheGreedyCapitalist() {
-        System.out.println("You're doing setting with me !?");
     }
 
     public TheGreedyCapitalist(Worker chineseDude) {
-        System.out.println("Ctor injection");
         this.chineseDude = chineseDude;
     }
 
     public void doSomethingSchemency() {
-
         chineseDude.doTheShit("TheGreedyCapitalist.java: The shit");
     }
 
     public void setChineseDude(Worker chineseDude) {
-        System.out.println("Setter injection");
         this.chineseDude = chineseDude;
     }
 
-/*
-    public static void main(String[] args) {
-        new TheGreedyCapitalist(
-                Worker.NO_OP_WORKER
-        ).doSomethingSchemency();
-
+    public void setCountry(String country) {
+        this.country = country;
     }
-*/
+
+    @Override
+    public String toString() {
+        return "TheGreedyCapitalist{" +
+                " from country='" + country + '\'' +
+                '}';
+    }
+
+    /*
+        public static void main(String[] args) {
+            new TheGreedyCapitalist(
+                    Worker.NO_OP_WORKER
+            ).doSomethingSchemency();
+
+        }
+    */
 }
--- spring-training/src/main/java/ro/teamnet/springtraining/beanfactory/ShyIntroToBeanFactory.java
+++ spring-training/src/main/java/ro/teamnet/springtraining/beanfactory/ShyIntroToBeanFactory.java
@@ -1,8 +1,7 @@
 package ro.teamnet.springtraining.beanfactory;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.BeanIsAbstractException;
 import ro.teamnet.springtraining.TheGreedyCapitalist;
 
 /**
@@ -12,21 +11,50 @@ import ro.teamnet.springtraining.TheGreedyCapitalist;
  * Time: 11:25 AM
  * TNI
  */
+@SuppressWarnings("deprecation")
 public class ShyIntroToBeanFactory {
 
     public static void main(String[] args) {
-        final ClassPathResource resource = new ClassPathResource("capitalists.xml", ShyIntroToBeanFactory.class);
+        BeanFactory beanFactory = BeanFactories.from("capitalists.xml", ShyIntroToBeanFactory.class);
 
-        BeanFactory beanFactory = new XmlBeanFactory(resource);
-        /*final TheGreedyCapitalist capitalist = beanFactory.getBean("capitalist", TheGreedyCapitalist.class);*/
+        exercisePrototypeScope(beanFactory);
+        exerciseMixInstantiation(beanFactory);
+        exerciseAbstractBeanDefinition(beanFactory);
+
 
         /*
         getBean(Class) -> aduce beanul care e definit UNIC de tipul asta , arunca NoSuchBeanException
         final TheGreedyCapitalist capitalist = beanFactory.getBean(TheGreedyCapitalist.class);
         */
 
+        /*capitalist.doSomethingSchemency();*/
+    }
+
+    private static void exerciseAbstractBeanDefinition(BeanFactory beanFactory) {
+        System.out.println("=== Abstract Bean Defintion ===");
+        try {
+            beanFactory.getBean("capitalist_abstract", TheGreedyCapitalist.class);
+        } catch (BeanIsAbstractException e) {
+            System.out.println("This is expected, it's abstract isn't it ?");
+        }
+
+        TheGreedyCapitalist capitalist = beanFactory.getBean("capitalist_concret", TheGreedyCapitalist.class);
+        System.out.println(capitalist);
+
+
+    }
+
+    private static void exerciseMixInstantiation(BeanFactory beanFactory) {
+        System.out.println("=== Mix Instantiation Example ===");
         final TheGreedyCapitalist capitalist_with_setter = beanFactory.getBean("capitalist_mix", TheGreedyCapitalist.class);
-//        capitalist.doSomethingSchemency();
         capitalist_with_setter.doSomethingSchemency();
+    }
+
+    private static void exercisePrototypeScope(BeanFactory beanFactory) {
+        System.out.println("=== Prototype Scope Example ===");
+        final TheGreedyCapitalist capitalist = beanFactory.getBean("capitalist", TheGreedyCapitalist.class);
+        final TheGreedyCapitalist capitalist2 = beanFactory.getBean("capitalist", TheGreedyCapitalist.class);
+        System.out.println(capitalist);
+        System.out.println(capitalist2);
     }
 }
